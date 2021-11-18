e2be1364b4147f9d8ccb57e6fe054294e28f7e05
Hans Ashlock <hashlock@thousandeyes.com>
2021-11-18 10:28:13 -0800

    agent info, wifi, network, use cookies

--- client/js/common.js
+++ client/js/common.js
@@ -1,36 +1,110 @@
-async function teEndpointMetric (testId) {
-  console.log("TE Endpoint");
-  $('#teResponse').html("Querying ThousandEyes...");
-  // $('#teMetric').html(`- ms`);
-  // $('#teLatency').html(`- ms`);
-  // $('#teMetric').css("background-color", "#F2F2F2");
-  var r1 = Math.floor(Math.random() * 10);
-  var r2 = Math.floor(Math.random() * 10);
-  var r3 = 90 + Math.floor(Math.random() * 10);
-  // $('#teMetric').html('<img  src="images/loading.gif" height="25px" width="25px">');
-  // $("#display_loading").show();
-  // getTEDate();
-  var resp = $.ajax({
-    // https://jur5bq3i8a.execute-api.us-east-1.amazonaws.com/dev/endpoint
-    url: `${metricsURL}/http`,
+async function teAgentInfo (hostName, accountGroupName, testName) {
+  console.log("Querying /agentinfo...");
+  let agentInfo = {};
+  await $.ajax({
+    url: `${metricsURL}/agentinfo?hostName=${hostName}&accountGroupName=${accountGroupName}&testName=${testName}`,
     method: "GET",
     crossDomain : true
   })
   .done(function(msg) {
-    var metric = msg['message']['endpointWeb']['httpServer'][0]['totalTime'];
-    $('#teServerResponse').html(`${metric}.${r1} ms`);
-    $('#teServerResponse').css("background-color", "greenyellow");
-    document.getElementById('teHealth').classList.add('green');
-    $('#teLatency').html(`98.${r2} ms`);
-    $('#teWifi').html(`${r3} %`);
-    // 
-    // var json = JSON.stringify(msg);
-    $('#teResponse').html(metric);
+    setCookie ("accountId", msg.message.accountId, 999);
+    setCookie ("agentId", msg.message.agentId, 999);
+    setCookie ("testId", msg.message.testId, 999);
+    agentInfo = msg.message;
   })
   .fail(function (msg) {
-    var json = JSON.stringify(msg);
+    console.log("ThousandEyes /agentinfo failed: " + JSON.stringify(msg));
+  });
+
+  return agentInfo
+}
+
+async function teEndpointMetric (accountId, agentId, testId) {
+  console.log("Querying ThousandEyes metrics...");
+  $.ajax({
+    url: `${metricsURL}/http?aid=${accountId}&agentId=${agentId}&testId=${testId}`,
+    method: "GET",
+    crossDomain : true
+  })
+  .done(function(msg) {
+    var metric = msg.message.totalTime;
+    $('#teServerResponse').html(`${metric} ms`);
+    switch (metric) {
+      case metric > 1000:
+        $('#teServerResponse').css("background-color", "red");
+        break;
+      case metric > 500:
+        $('#teServerResponse').css("background-color", "yellow");
+        break;
+      default:
+        $('#teServerResponse').css("background-color", "greenyellow");
+        break;
+    }
+    document.getElementById('teHealth').classList.add('green');
+  })
+  .fail(function (msg) {
+    console.log("ThousandEyes query failed with response: " + JSON.stringify(msg));
+  });
+
+  $.ajax({
+    url: `${metricsURL}/connection?aid=${accountId}&agentId=${agentId}&testId=${testId}`,
+    method: "GET",
+    crossDomain : true
+  })
+  .done(function(msg) {
+    if (msg.message.wirelessProfile) {
+      var quality = msg.message.wirelessProfile.quality;
+      $('#teWifi').html(`${msg.message.wirelessProfile.quality} %`);
+      switch (quality) {
+        case quality < 50:
+          $('#teWifi').css("background-color", "red");
+          break;
+        case quality < 75:
+          $('#teWifi').css("background-color", "yellow");
+          break;
+        default:
+          $('#teWifi').css("background-color", "greenyellow");
+          break;
+      }
+    }
+    else if (msg.message.hardwareType) {
+      $('#teWifi').html(`${msg.message.hardwareType}`);
+      $('#teWifi').css("background-color", "greenyellow");
+    }
+  })
+  .fail(function (msg) {
     console.log("Failed with response: " + JSON.stringify(msg));
-    $('#teResponse').html(json);
+  });
+
+
+  $.ajax({
+    url: `${metricsURL}/network?aid=${accountId}&agentId=${agentId}&testId=${testId}`,
+    method: "GET",
+    crossDomain : true
+  })
+  .done(function(msg) {
+    if (msg.message.avgLatency) {
+      var latency = msg.message.avgLatency;
+      $('#teLatency').html(`${latency} mS`);
+      switch (latency) {
+        case latency > 300:
+          $('#teLatency').css("background-color", "red");
+          break;
+        case latency > 100:
+          $('#teLatency').css("background-color", "yellow");
+          break;
+        default:
+          $('#teLatency').css("background-color", "greenyellow");
+          break;
+      }
+    } else {
+      $('#teLatency').html(`- mS`);
+      $('#teLatency').css("background-color", "#f2f2f2");
+      console.log (msg.message)
+    }
+  })
+  .fail(function (msg) {
+    console.log("Failed with response: " + JSON.stringify(msg));
   });
 }
 
@@ -42,8 +116,6 @@ async function teStatus () {
     url: `${metricsURL}/status`,
     method: "GET",
     crossDomain : true,
-    // user: user,
-    // password: password
   })
   .done(function(msg) {
     var json = JSON.stringify(msg);
@@ -55,252 +127,52 @@ async function teStatus () {
   
 }
 
-function storageGet(keys) {
-  return new Promise(function (resolve, reject) {
-    chrome.storage.local.get(keys, function (items) {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError.message);
-      } else {
-        if (items[keys] === undefined) {
-          reject("Key does not exist in local storage.");
-        } else {
-          resolve(items[keys]);
-        }
-      }
-    });
-  });
+// Storing values locally allows us to set/get cookie values 
+// when running in a local dev environment.
+let cookieCache = {};
+
+function setCookie (name, value, days = null) {
+  console.log (`Setting cookie ${name} to ${value}`);
+  var expires = "";
+  cookieCache[name] =  value;
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
 
-/*
- * Loads all (default) or a specific (provided by tag) templates json file(s) and parses them.
- */
-async function loadTemplates(tag = null) {
-  let templates = {};
-
-  let packageDir = await getPackageDirectoryEntry();
-  let packageDirChildren = await listDirectoryChildren(packageDir);
-  let templatesDir;
-  for (let i = 0; i < packageDirChildren.length; i++) {
-    let child = packageDirChildren[i];
-    if (child.isDirectory) {
-      if (child.name == "templates") {
-        templatesDir = child;
-      }
-    }
-  }
-  if (templatesDir === undefined) {
-    throw new Error("Templates directory is missing.");
+function getCookie (name) {
+  // console.log (`Getting cookie ${name}...`);
+  var val = null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) val = c.substring(nameEQ.length,c.length);
   }
 
-  let templatesDirChildren = await listDirectoryChildren(templatesDir);
-  let templateFiles = [];
-  for (let i = 0; i < templatesDirChildren.length; i++) {
-    let child = templatesDirChildren[i];
-    if (child.isFile) {
-      if (child.name.split(".").pop() == "json") {
-        if (tag == null || tag == child.name.split(".").slice(0, -1).join("."))
-          templateFiles.push(child);
-      }
-    }
+  // Check the cache. 
+  if (!val) {
+    val = cookieCache[name];
   }
-
-  for (let i = 0; i < templateFiles.length; i++) {
-    let fileText;
-    try {
-      fileText = await readFile(packageDir, templateFiles[i]);
-    } catch (e) {
-      throw new Error("Error opening templates/" + templateFiles[i].name);
-    }
-    let fileJson;
-    try {
-      fileJson = JSON.parse(fileText);
-    } catch (e) {
-      if (e instanceof SyntaxError) {
-        throw new Error(
-          e.message + " of " + templateFiles[i].name + ":\n" + fileText
-        );
-      } else {
-        throw new Error(e);
-      }
-    }
-    let key = templateFiles[i].name.split(".").slice(0, -1).join(".");
-    templates[key] = fileJson;
-  }
-
-  return new Promise(function (resolve, reject) {
-    if (
-      templates !== undefined &&
-      Object.getOwnPropertyNames(templates).length > 0
-    ) {
-      resolve(templates);
-    } else {
-      reject("No valid templates are present in templates directory.");
-    }
-  });
+  return val;
 }
 
-function makeRequest(url, method, headers, payload) {
-  var request = new XMLHttpRequest();
-  request.name = name;
-  return new Promise(function (resolve, reject) {
-    // Setup our listener to process compeleted requests
-    request.onreadystatechange = function () {
-      // Only run if the request is complete
-      if (request.readyState !== 4) return;
-      // Process the response
-      if (request.status >= 200 && request.status < 300) {
-        // If successful
-        try {
-          request.responseJSON = JSON.parse(request.responseText);
-        } catch { }
-        resolve(request);
-      } else {
-        // If failed
-        try {
-          request.responseJSON = JSON.parse(request.responseText);
-        } catch { }
-        reject(request);
-      }
-    };
-    request.open(method || "GET", url, true);
-    for (key in headers) {
-      request.setRequestHeader(key, headers[key]);
-    }
-    request.send(payload);
-  });
-}
-
-async function makeApiRequest(urlPath, method, aid, json) {
-  let url = "https://api.thousandeyes.com" + urlPath;
-  let apiCredentials = await storageGet(["apiCredentials"]);
-  let headers = {
-    Authorization:
-      "Basic " + btoa(apiCredentials.email + ":" + apiCredentials.token),
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  };
-  if (aid !== undefined && aid > 0) {
-    let u = new URL(url);
-    u.searchParams.append("aid", aid);
-    url = u.href;
-  }
-  let payload = json;
-  if (typeof json != "string") {
-    payload = JSON.stringify(json);
-  }
-
-  return makeRequest(url, method, headers, payload).catch(async function (
-    rejectedRequest
-  ) {
-    if (rejectedRequest.status == 429) {
-      showRateWarning(true);
-      let te_date, d;
-      await getTEDate()
-        .then((time) => {
-          te_date = Math.round(time / 1000);
-        })
-        .catch((r) => {
-          d = Math.round(Date.now() / 1000);
-        });
-
-      let resetTime = rejectedRequest.getResponseHeader(
-        "X-Organization-Rate-Limit-Reset"
-      );
-      let resetSeconds = resetTime - (te_date || d) + 1;
-      console.log(
-        "API Rate limit reached. Retrying once in " + resetSeconds + " seconds."
-      );
-      await new Promise((r) => setTimeout(r, resetSeconds * 1000));
-      showRateWarning(false);
-      return makeRequest(url, method, headers, payload);
-    } else {
-      throw rejectedRequest;
-    }
-  });
-}
-
-function getTEDate() {
-  let request = new XMLHttpRequest();
-  return new Promise(function (resolve, reject) {
-    // Setup our listener to process compeleted requests
-    request.onreadystatechange = function () {
-      // Only run if the request is complete
-      if (request.readyState !== 4) return;
-      // Process the response
-      if (request.status == 200) {
-        // If successful
-        request.responseJSON = JSON.parse(request.responseText);
-        resolve(request.responseJSON["timestamp"]);
-      } else {
-        reject(request);
-      }
-    };
-    // request.open("GET", "https://api.thousandeyes.com/v6/status.json", true);
-    // 
-    request.open("GET", `${metricsURL}`, true);
-    request.send();
-  });
-}
-
-/*
- * Async wrappers for chrome.runtime methods
- */
-
-function getPackageDirectoryEntry() {
-  return new Promise((resolve, reject) => {
-    chrome.runtime.getPackageDirectoryEntry((root) => {
-      if (root) {
-        resolve(root);
-      } else {
-        reject();
-      }
-    });
-  });
-}
-
-/*
- * Higher level async abstractions of chrome.runetime methods
- */
-
-function listDirectoryChildren(directoryEntry) {
-  return new Promise((resolve, reject) => {
-    let dirReader = directoryEntry.createReader();
-    dirReader.readEntries((children) => {
-      if (children) {
-        resolve(children);
-      } else {
-        reject();
-      }
-    });
-  });
-}
-
-function readFile(packageDir, file) {
-  return new Promise((resolve, reject) => {
-    packageDir.getFile(file.fullPath, {}, function (fileEntry) {
-      fileEntry.file(function (f) {
-        let reader = new FileReader();
-        reader.onloadend = function (e) {
-          if (e.target.result && e.target) {
-            resolve(e.target.result);
-          } else {
-            reject(e);
-          }
-        };
-        reader.onerror = function (e) {
-          reject(e);
-        };
-        reader.onabort = function (e) {
-          reject(e);
-        };
-        reader.readAsText(f);
-      });
-    });
-  });
-}
-
-function setVersion() {
-  document.getElementById("foot").innerHTML = document
-    .getElementById("foot")
-    .innerHTML.replace(/VERSION/g, "v" + chrome.runtime.getManifest().version);
-}
+// function storageGet(keys) {
+//   return new Promise(function (resolve, reject) {
+//     chrome.storage.local.get(keys, function (items) {
+//       if (chrome.runtime.lastError) {
+//         reject(chrome.runtime.lastError.message);
+//       } else {
+//         if (items[keys] === undefined) {
+//           reject("Key does not exist in local storage.");
+//         } else {
+//           resolve(items[keys]);
+//         }
+//       }
+//     });
+//   });
+// }
